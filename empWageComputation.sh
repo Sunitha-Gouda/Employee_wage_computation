cc6013b7ff0fcd89cf1bb8f6a5386266e261f0a3
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-06 14:04:17 +0530

    Employee Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,11 +3,15 @@ echo " Welcome to Employee Wage Computation Program "
 
 #variables
 IS_PRESENT=1;
+EMP_WAGE_PER_HR=20;
+EMP_HR=8;
 
 EMP_CHECK=$((RANDOM%2))
 if [ $EMP_CHECK -eq $IS_PRESENT ]
 then
 	echo " Employee Is Present "
+		Salary=$(($EMP_WAGE_PER_HR * $EMP_HR))
+	echo " Salary = " $Salary
 else
 	echo " Employee Is Absent "
 fi

b4b8fbd2452e8bab53fa67844c124f4cfc186ecb
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-06 14:40:56 +0530

    Wages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,19 +5,25 @@ echo " Welcome to Employee Wage Computation Program "
 IS_PART_TIME=1;
 IS_FULL_TIME=2
 EMP_WAGE_PER_HR=20;
+NUM_OF_WORKING_DAYS=20;
 
-EMP_CHECK=$((RANDOM%3))
-case $EMP_CHECK  in
-	$IS_FULL_TIME)
-			empHrs=8
-			;;
-	$IS_PART_ITME)
-			empHrs=4
-			;;
-	*)
-			empHrs=0
-			;;
-esac
+for (( Days=1; Days<=$NUM_OF_WORKING_DAYS; Days++ ))
+do
+	EMP_CHECK=$((RANDOM%3))
+		case $EMP_CHECK  in
+				$IS_FULL_TIME)
+							empHrs=8
+							;;
+				$IS_PART_ITME)
+							empHrs=4
+							;;
+				*)
+							empHrs=0
+							;;
+		esac
 
-Salary=$(($EMP_WAGE_PER_HR * $empHrs ))
-echo " Salary = " $Salary
+		Salary=$(( $NUM_OF_WORKING_DAYS * $empHrs ))
+			echo " Salary = " $Salary
+		TotalSalary=$(( $TotalSalary + $Salary ))
+			echo $TotalSalary
+done

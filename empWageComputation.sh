7853a5b0f79e34bde0045348efe0c13744e27e3a
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-06 14:59:27 +0530

    Wages for 100hrs and 20days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,24 +6,30 @@ IS_PART_TIME=1;
 IS_FULL_TIME=2
 EMP_WAGE_PER_HR=20;
 NUM_OF_WORKING_DAYS=20;
+MAX_HRS_IN_MONTH=100;
+EMP_CHECK=$((RANDOM%3))
 
-for (( Days=1; Days<=$NUM_OF_WORKING_DAYS; Days++ ))
+#constants
+totalEmpHr=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_OF_WORKING_DAYS ]]
 do
-	EMP_CHECK=$((RANDOM%3))
+   ((totalWorkingDays++))
 		case $EMP_CHECK  in
-				$IS_FULL_TIME)
-							empHrs=8
-							;;
-				$IS_PART_ITME)
-							empHrs=4
-							;;
-				*)
-							empHrs=0
-							;;
-		esac
+            $IS_FULL_TIME)
+                     empHrs=8
+                     ;;
+            $IS_PART_ITME)
+                     empHrs=4
+                     ;;
+            *)
+                     empHrs=0
+                     ;;
+      esac
 
-		Salary=$(( $NUM_OF_WORKING_DAYS * $empHrs ))
-			echo " Salary = " $Salary
-		TotalSalary=$(( $TotalSalary + $Salary ))
-			echo $TotalSalary
+
+totalEmpHr=$(( $totalEmpHr + $empHrs ))
 done
+		TotalSalary=$(( $totalEmpHr * $EMP_WAGE_PER_HR ))
+			echo $TotalSalary
